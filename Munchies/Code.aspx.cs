84a1ec759496209f9fa0e37440bdd6588d01cc9d
Wesley Cabus <wesley@gotsharp.be>
2015-03-29 15:54:57 +0200

    Added security demo code

---
+++ Munchies/Code.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Munchies.Admin
+{
+    public partial class Code : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (User.Identity.IsAuthenticated && User.IsInRole("Admin"))
+            {
+                // Do Admin Stuff
+            }
+            else
+            {
+                Response.Redirect("~/Default.aspx", false);
+            }
+        }
+    }
+}
